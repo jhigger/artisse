45153eabe34d6ed01691c2b016c26dc592877076
jhigger <njohnjhigger@gmail.com>
2023-06-25 15:43:59 +0800

    fix redirect

--- next.config.mjs
+++ next.config.mjs
@@ -6,18 +6,27 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	/**
+	 * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+	 * out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	async redirects() {
+		return [
+			{
+				source: "/exclusive",
+				destination: "https://9yzkecerap7.typeform.com/to/NlCsLOld",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default config;
