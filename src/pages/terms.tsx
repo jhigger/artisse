87af281180c655b4de4767e24cd11adf06b65ed6
jhigger <njohnjhigger@gmail.com>
2023-06-26 10:22:33 +0800

    terms of service

---
+++ src/pages/terms.tsx
@@ -0,0 +1,34 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import md from "~/markdowns/tos.md";
+
+const Terms = () => {
+	return (
+		<div className="container mx-auto my-32 w-max">
+			<ReactMarkdown
+				components={{
+					ol: ({ depth, ...props }) => {
+						if (depth === 1) {
+							return (
+								<ol
+									style={{ listStyleType: "lower-alpha" }}
+									{...props}
+								/>
+							);
+						}
+
+						return <ol {...props} />;
+					},
+				}}
+				remarkPlugins={[remarkGfm]}
+				rehypePlugins={[rehypeRaw]}
+				className="prose"
+			>
+				{md}
+			</ReactMarkdown>
+		</div>
+	);
+};
+
+export default Terms;
