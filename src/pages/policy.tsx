5560df18d9038ef5a2ee188955f5f581dc39805d
jhigger <njohnjhigger@gmail.com>
2023-06-26 11:28:01 +0800

    head title

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -2,18 +2,31 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import md from "~/assets/markdown/pp.md";
+import Head from "next/head";
 
 const Policy = () => {
 	return (
-		<div className="container mx-auto my-32 w-max">
-			<ReactMarkdown
-				remarkPlugins={[remarkGfm]}
-				rehypePlugins={[rehypeRaw]}
-				className="prose"
-			>
-				{md}
-			</ReactMarkdown>
-		</div>
+		<>
+			<Head>
+				<title>Artisse AI | Privacy Policy</title>
+				<meta
+					name="description"
+					content="Artisse AI - Changing photography forever"
+				/>
+				<link rel="canonical" href="https://artisse.ai/" />
+				<meta name="author" content="https://github.com/jhigger" />
+				<link rel="icon" href="/artisse favicon.png" />
+			</Head>
+			<div className="container mx-auto my-32 w-max">
+				<ReactMarkdown
+					remarkPlugins={[remarkGfm]}
+					rehypePlugins={[rehypeRaw]}
+					className="prose"
+				>
+					{md}
+				</ReactMarkdown>
+			</div>
+		</>
 	);
 };
 
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -2,33 +2,46 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import md from "~/assets/markdown/tos.md";
+import Head from "next/head";
 
 const Terms = () => {
 	return (
-		<div className="container mx-auto my-32 w-max">
-			<ReactMarkdown
-				components={{
-					// eslint-disable-next-line @typescript-eslint/no-unused-vars
-					ol: ({ depth, ordered, ...props }) => {
-						if (depth === 1) {
-							return (
-								<ol
-									style={{ listStyleType: "lower-alpha" }}
-									{...props}
-								/>
-							);
-						}
+		<>
+			<Head>
+				<title>Artisse AI | Terms of Service</title>
+				<meta
+					name="description"
+					content="Artisse AI - Changing photography forever"
+				/>
+				<link rel="canonical" href="https://artisse.ai/" />
+				<meta name="author" content="https://github.com/jhigger" />
+				<link rel="icon" href="/artisse favicon.png" />
+			</Head>
+			<div className="container mx-auto my-32 w-max">
+				<ReactMarkdown
+					components={{
+						// eslint-disable-next-line @typescript-eslint/no-unused-vars
+						ol: ({ depth, ordered, ...props }) => {
+							if (depth === 1) {
+								return (
+									<ol
+										style={{ listStyleType: "lower-alpha" }}
+										{...props}
+									/>
+								);
+							}
 
-						return <ol {...props} />;
-					},
-				}}
-				remarkPlugins={[remarkGfm]}
-				rehypePlugins={[rehypeRaw]}
-				className="prose"
-			>
-				{md}
-			</ReactMarkdown>
-		</div>
+							return <ol {...props} />;
+						},
+					}}
+					remarkPlugins={[remarkGfm]}
+					rehypePlugins={[rehypeRaw]}
+					className="prose"
+				>
+					{md}
+				</ReactMarkdown>
+			</div>
+		</>
 	);
 };
 
